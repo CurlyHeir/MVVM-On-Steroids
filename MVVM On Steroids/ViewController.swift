e687b16440492b1e39344ac40d16a1c9ccb16d95
Przemysław Wośko <przemyslaw.wosko@intive.com>
2017-10-16 00:47:00 +0200

    create project

---
+++ MVVM On Steroids/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MVVM On Steroids
+//
+//  Created by przemyslaw.wosko on 16/10/2017.
+//  Copyright © 2017 org.patterns.mvvm. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+
+}
+
---
+++ MVVM On SteroidsTests/MVVM_On_SteroidsTests.swift
@@ -0,0 +1,18 @@
+//
+//  MVVM_On_SteroidsTests.swift
+//  MVVM On SteroidsTests
+//
+//  Created by przemyslaw.wosko on 16/10/2017.
+//  Copyright © 2017 org.patterns.mvvm. All rights reserved.
+//
+
+import XCTest
+@testable import MVVM_On_Steroids
+
+class MVVM_On_SteroidsTests: XCTestCase {
+    
+    func test_example() {
+
+    }
+    
+}
